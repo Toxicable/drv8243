b6f3e5399951da1023d4d2671708ba98134e1dde
Fabian Wiles <fabianwiles@canva.com>
2025-12-30 23:58:16 +0000

    a

--- components/drv8243/drv8243.cpp
+++ components/drv8243/drv8243.cpp
@@ -77,19 +77,19 @@ void DRV8243Output::setup() {
 }
 
 void DRV8243Output::run_handshake(const char *reason) {
-//   ESP_LOGW(TAG, "run_handshake(): requested (reason=%s)", reason);
+  ESP_LOGW(TAG, "run_handshake(): requested (reason=%s)", reason);
 
-//   // (Optional) make sure PWM is off while we do the handshake to avoid flicker
-//   if (raw_output_ != nullptr) {
-//     ESP_LOGI(TAG, "run_handshake(): forcing raw_output OFF (0.0) before handshake");
-//     raw_output_->set_level(0.0f);
-//   }
+  // (Optional) make sure PWM is off while we do the handshake to avoid flicker
+  if (raw_output_ != nullptr) {
+    ESP_LOGI(TAG, "run_handshake(): forcing raw_output OFF (0.0) before handshake");
+    raw_output_->set_level(0.0f);
+  }
 
-//   bool ok = this->do_handshake_(reason);
-//   this->handshaked_ = true;
-//   this->handshake_ok_ = ok;
+  bool ok = this->do_handshake_(reason);
+  this->handshaked_ = true;
+  this->handshake_ok_ = ok;
 
-//   ESP_LOGW(TAG, "run_handshake(): done ok=%s", ok ? "true" : "false");
+  ESP_LOGW(TAG, "run_handshake(): done ok=%s", ok ? "true" : "false");
 }
 
 void DRV8243Output::pulse_nsleep_ack_() {
